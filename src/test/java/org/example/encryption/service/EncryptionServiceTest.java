415a46243730a8649f8cd31a65176202c3cc3658
Suchit Charaschanya <Suchit.Charaschanya@ibm.com>
2022-07-06 21:07:55 +0700

    Minor fixes

--- src/test/java/org/example/encryption/service/EncryptionServiceTest.java
+++ src/test/java/org/example/encryption/service/EncryptionServiceTest.java
@@ -6,19 +6,25 @@ import org.junit.Test;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class EncryptionServiceTest {
     IEncryptionService iEncryptionService = new EncryptionService();
+    String plainString = "plainString";
+    String key = "dRgUkXp2s5v8y/A?D(G+KbPeShVmYq3t";
 
     @Test
     public void decryptShouldBeTheInverseOfEncrypt() throws Exception {
-        String plainString = "plainString";
-        String key = "dRgUkXp2s5v8y/A?D(G+KbPeShVmYq3t";
-
         byte[] encryptedBytes = iEncryptionService.encrypt(plainString.getBytes(StandardCharsets.UTF_8), key);
         byte[] decryptedBytes = iEncryptionService.decrypt(encryptedBytes, key);
         String decryptedString = new String(decryptedBytes, StandardCharsets.UTF_8);
-
         assertEquals(decryptedString, plainString);
+    }
+
+    @Test
+    public void encryptingTwiceWithSameKeyShouldBeDifferent() throws Exception {
+        byte[] encryptedBytes1 = iEncryptionService.encrypt(plainString.getBytes(StandardCharsets.UTF_8), key);
+        byte[] encryptedBytes2 = iEncryptionService.encrypt(plainString.getBytes(StandardCharsets.UTF_8), key);
+        assertNotEquals(encryptedBytes1, encryptedBytes2);
     }
 }
