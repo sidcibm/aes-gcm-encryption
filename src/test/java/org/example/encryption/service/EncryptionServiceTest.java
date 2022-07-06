a1f176b1fb2a073647539adb3897b8bb59749a1d
Suchit Charaschanya <Suchit.Charaschanya@ibm.com>
2022-07-06 20:57:39 +0700

    Minor fixes

--- src/test/java/org/example/encryption/service/EncryptionServiceTest.java
+++ src/test/java/org/example/encryption/service/EncryptionServiceTest.java
@@ -1,4 +1,24 @@
 package org.example.encryption.service;
 
+import org.example.encryption.service.impl.EncryptionService;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
 public class EncryptionServiceTest {
+    IEncryptionService iEncryptionService = new EncryptionService();
+
+    @Test
+    public void decryptShouldBeTheInverseOfEncrypt() throws Exception {
+        String plainString = "plainString";
+        String key = "dRgUkXp2s5v8y/A?D(G+KbPeShVmYq3t";
+
+        byte[] encryptedBytes = iEncryptionService.encrypt(plainString.getBytes(StandardCharsets.UTF_8), key);
+        byte[] decryptedBytes = iEncryptionService.decrypt(encryptedBytes, key);
+        String decryptedString = new String(decryptedBytes, StandardCharsets.UTF_8);
+
+        assertEquals(decryptedString, plainString);
+    }
 }
